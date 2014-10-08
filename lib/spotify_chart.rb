b706536b712d441f9d8404535218d3c21439941a
Rose Weixel <rose.weixel@gmail.com>
2014-10-08 13:56:43 -0400

    Made changes

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,24 +1,59 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
 
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
     # return a string that is the base url + / + preference + / + region + / + latest
+    "#{@base_url}#{preference}/#{region}/latest"
   end
 
   def get_json(url)
     # load json given a url here
-    # refer to the references if you have questions about this
+    #JSON.(`curl #{url}`)
+    JSON.load(open(url))
   end
 
   def fetch_track_album_artist(music_hash)
+    track_name = music_hash["tracks"].first["track_name"]
+    artist_name = music_hash["tracks"].first["artist_name"]
+    album_name = music_hash["tracks"].first["album_name"]
+    "'#{track_name}' by #{artist_name} from the album #{album_name}"
+  end
+
+  def most_streamed(region)
+    # call no get_url here, where preference is the string 'most_streamed',
+    # and set it equal to a variable
+    most_streamed_url = get_url('most_streamed', region)
+    # call on get_json here, using the string that get_url returns
+    hash = get_json(most_streamed_url)
+    binding.pry
+    # finally, call on fetch_track_album_artist using the 
+    # hash that get_json returns
+    fetch_track_album_artist(hash)
+    #binding.pry
+  end
+
+  def most_shared(region)
+    # call no get_url here, where preference is the string 'most_shared',
+    # and set it equal to a variable
+
+    # call on get_json here, using the string that get_url returns
+    
+    # finally, call on fetch_track_album_artist using the 
+    # hash that get_json returns
+  end
+
+end
+
+
     # example music_hash:
     # {  
     #   "tracks" => [  
@@ -42,27 +77,3 @@ class SpotifyChart
   
     # the track name, artist name, and album name should be the first track in the
     # tracks array
-  end
-
-
-  def most_streamed(region)
-    # call no get_url here, where preference is the string 'most_streamed',
-    # and set it equal to a variable
-
-    # call on get_json here, using the string that get_url returns
-    
-    # finally, call on fetch_track_album_artist using the 
-    # hash that get_json returns
-  end
-
-  def most_shared(region)
-    # call no get_url here, where preference is the string 'most_shared',
-    # and set it equal to a variable
-
-    # call on get_json here, using the string that get_url returns
-    
-    # finally, call on fetch_track_album_artist using the 
-    # hash that get_json returns
-  end
-
-end
